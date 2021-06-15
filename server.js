a41e3b70e2e02e91fc16e3b86350da941f79d648
Charlie <charjhall777@gmail.com>
2021-06-15 00:47:52 -0400

    adding initial server tidbits

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config({
+	path: __dirname + "/.env"
+});
+const express = require('express');
+const mustache = require('mustache-express');
+const bodyParser = require('body-parser');
+const {
+	v4: uuidv4
+} = require('uuid');
+const morgan = require('morgan');
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	user: process.env.USER_NAME,
+	password: process.env.PASSWORD,
+	insecureAuth: false
+});
+
+connection.connect((err) => {
+	if (err) throw err;
+});

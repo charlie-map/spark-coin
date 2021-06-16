009ac089e559838d75132ed9ae45a9d895419680
Zach Minster <zach.minster@gmail.com>
2021-06-16 04:35:14 -0400

    (socket) coin transfer, (socket) item/raffle item purchasing, (admin) raffle :: working

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const {
 	v4: uuidv4
 } = require('uuid');
 
+
+
+const axios = require('axios');
 const mustache = require('mustache-express');
 const express = require('express');
 const app = express();
@@ -247,7 +250,51 @@ app.get("/admin/campers", isLoggedIn(2), (req, res, next) => {
 
 app.post("/admin/raffle", isLoggedIn(2), (req, res) => {
 	RAFFLE_MODE = !RAFFLE_MODE;
-	res.end(RAFFLE_MODE);
+	res.end(""+RAFFLE_MODE);
+});
+
+app.get("/admin/raffle/drawing", isLoggedIn(2), async (req, res, next) => {
+	try {
+		// get all raffle items
+		let raffle_items = await new Promise ((resolve, reject) => {
+			connection.query("SELECT id, item_name, description FROM raffle_item WHERE active = 1;", (err, result) => {
+				if (err) reject(err);
+				resolve(result);
+			});
+		});
+
+		// select raffle winner (-1 if nobody bought tickets)
+		let winners = raffle_items.map((item) => {
+			return new Promise((resolve, reject) => {
+				connection.query("SELECT sender_id FROM tx WHERE raffle_item = ?;", [item.id], (err, result) => {
+					if (err) reject(err);
+					if (!result || !result.length) { item.winner = -1; return resolve(item); }
+					item.winner = result[Math.floor(Math.random() * result.length)].sender_id;
+					return resolve(item);
+				});
+			});
+		});
+		winners = await Promise.all(winners);
+
+		// augment raffle items with winner data
+		let augmented_winners = winners.map((item) => {
+			return new Promise((resolve, reject) => {
+				if (item.winner == -1) {
+					item.winner_name = "Nobody Won";
+					return resolve(item);
+				}
+				connection.query("SELECT first_name, last_name FROM registration.camper WHERE id = ?;", [item.winner], (err, result) => {
+					if (err) reject(err);
+					item.winner_name = result[0].first_name + ' ' + result[0].last_name;
+					return resolve(item);
+				});
+			});
+		});
+		augmented_winners = await Promise.all(augmented_winners);
+		res.json(augmented_winners);
+	} catch (err) {
+		next(err);
+	}
 });
 
 app.get("/admin/log", isLoggedIn(2), (req, res, next) => {
@@ -300,10 +347,127 @@ io.on('connection', (socket) => {
 		sess_store.get(sid, (err, data) => { socket.user = data.user; user_sockets[data.user.camper_id] = socket; });
 
 	socket.on('inventory_get', (cb) => {
+		// if raffle, send raffle items instead
+		let query_string;
+		if (RAFFLE_MODE)
+			query_string = "SELECT id, item_name, description, image_url, 'RAFFLE' AS owner FROM raffle_item WHERE active = 1;";
+		else
+			query_string = "SELECT id, item_name, description, image_url, camp_name AS owner, price FROM inventory LEFT JOIN spark_user ON inventory.camper_id = spark_user.camper_id WHERE active = 1 AND quantity > 0;"
 		// filter out no quantity items & only active items
-		connection.query("SELECT id, item_name, description, image_url, camp_name AS owner, price FROM inventory LEFT JOIN spark_user ON inventory.camper_id = spark_user.camper_id WHERE active = 1 AND quantity > 0;", (err, result) => {
+		connection.query(query_string, (err, result) => {
 			cb(result);
 		});
+	});
+
+	socket.on('purchase', async (item_id, cb) => {
+		if (!socket.user || socket.user.staffer != 0) return cb("Not logged in / not correct role.");
+		try {
+			// get data about inventory item & verify quantity / active
+			let item = await new Promise((resolve, reject) => {
+				if (RAFFLE_MODE) {
+					connection.query("SELECT * FROM raffle_item WHERE id = ?;", [item_id], (err, result) => {
+						if (err) reject(err);
+						if (!result) reject("Invalid raffle item.");
+						if (result[0].active != 1) reject("This raffle item is unavailable.");
+						result[0].price = 1;
+						resolve(result[0]);
+					});
+				} else {
+					connection.query("SELECT * FROM inventory WHERE id = ?;", [item_id], (err, result) => {
+						if (err) reject(err);
+						if (!result) reject("Invalid item.");
+						if (result[0].quantity < 1 || result[0].active != 1) reject("This item is unavailable.");
+						resolve(result[0]);
+					});
+				}
+			});
+
+			// get/verify appropriate balance & quantity / active
+			let bal = await new Promise((resolve, reject) => {
+				connection.query("SELECT balance FROM spark_user WHERE camper_id = ?;", [socket.user.camper_id], (err, result) => {
+					if (err || !result) reject("Purchasing camper does not exist.");
+					if (result[0].balance < item.price) reject("Not enough Sparks.");
+					resolve(result[0].balance);
+				});
+			});
+
+			bal -= item.price;
+			bal = roundTo(bal, 3);
+
+			if (RAFFLE_MODE) {
+				await new Promise((resolve, reject) => {
+					connection.query("UPDATE spark_user SET balance = ? WHERE camper_id = ?;", [bal, socket.user.camper_id], (err) => {
+						if (err) reject(err);
+						connection.query("INSERT INTO tx (receiver_id, sender_id, inventory_item, raffle_item, amount, message, tx_time) VALUES (NULL, ?, NULL, ?, 1, NULL, ?);",
+							[ socket.user.camper_id, item.id, new Date()], (err) => {
+							if (err) reject(err);
+							resolve();
+						});
+					});
+				});
+				socket.emit('balance', bal, -1);
+				return cb(null);
+			}
+
+			// remove quantity / remove balance / log tx
+			await new Promise((resolve, reject) => {
+				connection.query("UPDATE inventory SET quantity = ? WHERE id = ?;", [item.quantity-1, item.id], (err) => {
+					if (err) reject(err);
+					connection.query("UPDATE spark_user SET balance = ? WHERE camper_id = ?;", [bal, socket.user.camper_id], (err) => {
+						if (err) reject(err);
+						connection.query("INSERT INTO tx (receiver_id, sender_id, inventory_item, raffle_item, amount, message, tx_time) VALUES (NULL, ?, ?, NULL, ?, NULL, ?);",
+							[ socket.user.camper_id, item.id, item.price, new Date()], (err) => {
+							if (err) reject(err);
+							resolve();
+						});
+					});
+				});
+			});
+
+			// notify all parties
+			socket.emit('balance', bal, -1);
+			let message = 'Item ' + item.item_name + ' was purchased by ' + socket.user.first_name + ' ' + socket.user.last_name + ' (#' + socket.user.camper_id + ')!' + ' There are ' + (item.quantity-1) + ' of these left.';
+			if (!item.camper_id) {	// camp-wide alert for camp-wide item
+				// loop through users and find all admins
+				let admin_slack_ids = await new Promise((resolve, reject) => {
+					connection.query("SELECT slack_id FROM spark_user WHERE staffer > 1;", (err, result) => {
+						if (err || !result)
+							resolve(null)
+						resolve(result);
+					});
+				});
+				await Promise.all(admin_slack_ids.map((slack_id) => {
+					axios.post('https://slack.com/api/chat.postMessage',
+					{
+						token: process.env.SLACK_TOKEN,
+						channel: slack_id.slack_id,
+						text: message
+					}, { headers: { authorization: 'Bearer ' + process.env.SLACK_TOKEN } });
+				}));
+				return cb(false);
+			} else {				// specific alert for staffer
+				if (user_sockets[item.camper_id]) user_sockets[item.camper_id].emit('alert', message);
+				// look up staffer's slack ID & message them
+				let slack_id = await new Promise((resolve, reject) => {
+					connection.query("SELECT slack_id FROM spark_user WHERE camper_id = ?;", [item.camper_id], (err, result) => {
+						if (err || !result)
+							resolve(null);
+						resolve(result[0].slack_id);
+					});
+				});
+				if (slack_id) {
+					await axios.post('https://slack.com/api/chat.postMessage',
+					{
+						token: process.env.SLACK_TOKEN,
+						channel: slack_id,
+						text: message
+					}, { headers: { authorization: 'Bearer ' + process.env.SLACK_TOKEN } });
+				}
+				return cb(false);
+			}
+		} catch (err) {
+			return cb(err);
+		}
 	});
 
 	socket.on('transfer', async (receiving_id, amount, message, cb) => {

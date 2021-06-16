200200d9976c4dcfaa37e4bc134db2d6b22a29e5
Charlie <charjhall777@gmail.com>
2021-06-15 23:34:55 -0400

    adding balance animations

--- public/initial.js
+++ public/initial.js
@@ -1,12 +1,60 @@
 const socket = io();
 
-function change_number() {
+$("#shake_balance").hide();
 
-	$("#balance_display").addClass("changing");
+let update_balance_shakers = $(".container").find(".shake_balance");
+
+for (let update = 0; update < update_balance_shakers.length; update++) {
+
+	let new_number = (Math.random() * 1300).toString();
+	new_number = new_number.substring(0, new_number.length - 10);
+
+	$(update_balance_shakers[update]).text(new_number);
 }
 
-change_number();
+function cut_css_number() {
+	$("#balance_display").removeClass("animate__shakeY");
+	if (num > 0) $("#balance_display").removeClass("changing-up");
+	else $("#balance_display").removeClass("changing-down");
 
-$("#balance_display").click(() => {
-	$("#balance_display").removeClass("changing");
+	$(".shake_balance").removeClass("animate__shakeY");
+	if (num > 0) $(".shake_balance").removeClass("changing-up");
+	else $(".shake_balance").removeClass("changing-down");
+}
+
+function invis_number() {
+	$("#balance_display").addClass("animate__shakeY");
+	$(".shake_balance").addClass("animate__shakeY");
+
+	setTimeout(cut_css_number, 250);
+}
+
+function show_number() { // and change actual number value
+	if (num > 0) $("#balance_display").text("420.69");
+	else $("#balance_display").text("69.42");
+	$("#balance_display").removeClass("animate__shakeY");
+	$(".shake_balance").removeClass("animate__shakeY");
+
+	setTimeout(invis_number, 250);
+}
+
+function change_number() {
+
+	if (num > 0) $("#balance_display").addClass("changing-up");
+	else $("#balance_display").addClass("changing-down");
+	$("#balance_display").addClass("animate__shakeY");
+	$(".shake_balance").addClass("animate__shakeY");
+
+	if (num > 0) $(".shake_balance").addClass("changing-up");
+	else $(".shake_balance").addClass("changing-down");
+
+	setTimeout(show_number, 250);
+}
+
+let num;
+
+$("#test").click(() => {
+	$(".shake_balance").css("left", $(".lightning-bolt").offset().left + 70 + "px");
+	num = 1;
+	change_number();
 });

281ff5312b5289e48f0701f7091f7855c52ea764
Charlie <charjhall777@gmail.com>
2021-06-15 22:01:56 -0400

    mustache change and initial home page ui

--- public/initial.js
+++ public/initial.js
@@ -1,1 +1,12 @@
 const socket = io();
+
+function change_number() {
+
+	$("#balance_display").addClass("changing");
+}
+
+change_number();
+
+$("#balance_display").click(() => {
+	$("#balance_display").removeClass("changing");
+});

536fe16f15e1b568f29105bdc60edff66b8e0576
Charlie <charjhall777@gmail.com>
2021-09-05 22:04:36 -0400

    db table updates

--- db_queries/CREATE_DB.sql
+++ db_queries/CREATE_DB.sql
@@ -9,25 +9,63 @@ CREATE TABLE settings (
 	PRIMARY KEY (name)	
 );
 
-INSERT INTO settings (name, value) VALUES ('raffle', 0);
-INSERT INTO settings (name, value) VALUES ('starter_coins', 10);
-INSERT INTO settings (name, value) VALUES ('staff_coins', 250);
-INSERT INTO settings (name, value) VALUES ('block_camper', 1);
+CREATE TABLE city (
+	id INT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	raffle_active TINYINT DEFAULT 0,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE market (
+	id INT NOT NULL,
+	city_id INT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	icon TEXT,
+	starter_coins INT NOT NULL,
+	staff_coins INT NOT NULL,
+	disabled TINYINT DEFAULT 0,
+	tx_blocked TINYINT DEFAULT 0,
+	raffle_active TINYINT DEFAULT 0,
+	PRIMARY KEY (id),
+	FOREIGN KEY (`city_id`) REFERENCES city (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE market_membership (
+	camper_id INT NOT NULL,
+	market_id INT NOT NULL,
+	staffer TINYINT NOT NULL,
+	camp_name VARCHAR(255),
+	FOREIGN KEY (`camper_id`) REFERENCES spark_user (`camper_id`) ON DELETE CASCADE,
+	FOREIGN KEY (`marker_id`) REFERENCES market (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE system (
+	id INT NOT NULL,
+	db_name VARCHAR(255),
+	name VARCHAR(255) NOT NULL,
+	system_type INT NOT NULL,
+	authable TINYINT DEFAULT 0,
+	market_id INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (`market_id`) REFERENCES market (`id`) ON DELETE CASCADE
+);
 
 CREATE TABLE spark_user (
-	camper_id INT NOT NULL,
-	staffer TINYINT,
+	camper_id INT NOT NULL UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
 	pin INT NOT NULL,
 	balance DOUBLE NOT NULL,
 	last_login DATETIME,
 	slack_id VARCHAR(255),
-	camp_name VARCHAR(255),
-	PRIMARY KEY (camper_id),
-	FOREIGN KEY (`camper_id`) REFERENCES registration.camper (`id`) ON DELETE CASCADE
+	PRIMARY KEY (camper_id)
 );
 
 CREATE TABLE inventory (
 	id INT AUTO_INCREMENT NOT NULL,
+	market_id INT,
+	city_id INT,
 	camper_id INT,
 	item_name VARCHAR(255) NOT NULL,
 	description TEXT,
@@ -36,16 +74,22 @@ CREATE TABLE inventory (
 	quantity INT,
 	active TINYINT,
 	PRIMARY KEY (id),
-	FOREIGN KEY (`camper_id`) REFERENCES registration.camper(`id`)
+	FOREIGN KEY (`market_id`) REFERENCES market (`id`),
+	FOREIGN KEY (`city_id`) REFERENCES city (`id`),
+	FOREIGN KEY (`camper_id`) REFERENCES spark_user (`camper_id`)
 );
 
 CREATE TABLE raffle_item (
 	id INT AUTO_INCREMENT NOT NULL,
+	market_id INT,
+	city_id INT,
 	item_name VARCHAR(255) NOT NULL,
 	description TEXT,
 	image_url TEXT,
 	active TINYINT,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (`market_id`) REFERENCES market (`id`),
+	FOREIGN KEY (`city_id`) REFERENCES city (`id`)
 );
 
 CREATE TABLE tx (
